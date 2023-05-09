63d01d7749f5bfe1eee87ab9ec291237b26b5ad0
tylervanderstaay <117237435+tylervanderstaay@users.noreply.github.com>
2023-05-09 01:03:35 -0400

    Started api routes

---
+++ routes/api/index.js.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const categoryRoutes = require('./category-routes');
+const userRoutes = require('./user-routes');
+const topicRoutes = require('./topic-routes');
+const commentRoutes = require('./comment-routes')
+
+router.use('/categories', categoryRoutes);
+router.use('/users', userRoutes);
+router.use('/topics', topicRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
---
+++ routes/api/post-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { User, Category, Topic, Post, Comment } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll();
+
+        res.status(200).json(categoryData);
+    } catch (err) {
+
+        res.status(500).json(err)
+    }
+});
