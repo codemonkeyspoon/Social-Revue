137f10eff68c5225104d5d1e607bdf31659ea845
tylervanderstaay <117237435+tylervanderstaay@users.noreply.github.com>
2023-05-09 01:06:39 -0400

    fixed a few errors

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,9 +5,11 @@ router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll();
 
-        res.status(200).json(categoryData);
+        res.status(200).json(postData);
     } catch (err) {
 
         res.status(500).json(err)
     }
 });
+
+module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
             include: [{model: Post}]
         });
 
-        res.status(200).json(categoryData);
+        res.status(200).json(userData);
     } catch (err) {
 
         res.status(500).json(err)
