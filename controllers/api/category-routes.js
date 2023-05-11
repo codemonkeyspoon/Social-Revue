50428cd47dba401d7c0e1b017d8869f4ac299b62
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-05-11 17:08:09 -0400

    category done

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try { 
         const categoryData = await Category.findByPk(req.params.id, {
-            include: [{model: Topic}]
+            include: [{model: Post}]
         });
 
         res.status(200).json(categoryData);
